0defd761ac46ddad292b88eae7018d1d94ddf5fa
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-06-11 12:43:25 +0530

    Create Q1.cpp

---
+++ Day_11/Q1.cpp
@@ -0,0 +1,47 @@
+// Problem: Intersection of Two Linked Lists
+// Link: https://leetcode.com/problems/intersection-of-two-linked-lists/
+// Approach: Use two pointers - fast and slow. Move them with different speeds. 
+// When they meet => 
+// if it is null => no intersection
+// else => they intersect
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        int len1 = 0, len2 = 0;
+        ListNode *temp1 = headA, *temp2 = headB;
+        while (temp1 != NULL) {
+            temp1 = temp1->next;
+            len1++;
+        }
+        while (temp2 != NULL) {
+            temp2 = temp2->next;
+            len2++;
+        }
+        temp1 = headA, temp2 = headB;
+        // Move both heads to same starting length
+        // This ensures that both lists have same length to compare
+        if (len1 < len2) {
+            for (int i = 0; i < len2 - len1; i++) {
+                temp2 = temp2->next;
+            }
+        } else {
+            for (int i = 0; i < len1 - len2; i++) {
+                temp1 = temp1->next;
+            }
+        }
+        // Keep moving until both reach any same node
+        while (temp1 != temp2) {
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        return temp1;
+    }
+};
