2ce0525ea9aba59158097ed36e785e8fa3dc65ad
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-06-30 22:52:08 +0530

    Create Q2.cpp

---
+++ Day_30/Q2.cpp
@@ -0,0 +1,22 @@
+// Problem: Longest Common Prefix
+// Link: https://leetcode.com/problems/longest-common-prefix/
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& str) {
+        if (str.empty()) {
+            return "";
+        }
+        sort(str.begin(), str.end());
+        string first = str[0];
+        string last = str[str.size() - 1];
+        string ans = "";
+        int minLength = min(first.size(), last.size());
+        for (int i = 0; i < minLength; i++) {
+            if (first[i] != last[i]) {
+                break;
+            }
+            ans += first[i];
+        }
+        return ans;
+    }
+};
