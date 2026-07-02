49c3a53b101f124cec6f7673cb9323147b6fe54e
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-07-02 18:34:32 +0530

    Create Q1.cpp

---
+++ Day_32/Q1.cpp
@@ -0,0 +1,13 @@
+// Problem: Valid Anagram
+// Link: https://leetcode.com/problems/valid-anagram/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
