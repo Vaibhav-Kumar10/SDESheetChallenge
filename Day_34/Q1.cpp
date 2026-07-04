b52d02cf6bc52bac4f78506fb958a998a6782996
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-07-04 22:09:37 +0530

    Create Q1.cpp

---
+++ Day_34/Q1.cpp
@@ -0,0 +1,46 @@
+// Problem: Morris Preorder Traversal 
+// Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void morrisPreorder(TreeNode* root, vector<int>& ans) {
+        TreeNode* cur = root;
+        while (cur) {
+            if (cur->left == NULL) {
+                ans.push_back(cur->val);
+                cur = cur->right;
+            } else {
+                TreeNode* prev = cur->left;
+
+                while (prev->right && prev->right != cur)
+                    prev = prev->right;
+
+                if (prev->right == NULL) {
+                    // Visit before going left
+                    ans.push_back(cur->val); 
+                    prev->right = cur;
+                    cur = cur->left;
+                } else {
+                    prev->right = NULL;
+                    cur = cur->right;
+                }
+            }
+        }
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        morrisPreorder(root, ans);
+        return ans;
+    }
+};
