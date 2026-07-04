bb9156174ca3ca0cb723a06838b7f5c0121ef51e
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-07-04 22:13:27 +0530

    Create Q2_2.cpp

---
+++ Day_34/Q2_2.cpp
@@ -0,0 +1,34 @@
+// Problem: Left View of Binary Tree
+// Link: https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1
+/* A binary tree node
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+class Solution {
+  public:
+    void solve(Node *root, vector<int> &ans, int level){
+        if(root == NULL) {
+            return;
+        }
+        if(ans.size() == level) {
+            ans.push_back(root->data);
+        }
+        solve(root->left, ans, level+1);
+        solve(root->right, ans, level+1);
+    }
+    vector<int> leftView(Node *root) {
+        vector<int> ans;
+        solve(root, ans, 0);
+        return ans;
+    }
+};
+
