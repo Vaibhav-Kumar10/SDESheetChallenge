142c024c4cd53b4f8536296ecf05c9394afb5118
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-07-10 21:30:04 +0530

    Create Q1.cpp

---
+++ Day_40/Q1.cpp
@@ -0,0 +1,45 @@
+// Problem: Populating Next Right Pointers in Each Node
+// Link: https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == NULL) {
+            return NULL;
+        }
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            Node* rightNode = NULL;
+            for (int i = q.size(); i; i--) {
+                auto cur = q.front();
+                q.pop();
+                cur->next = rightNode;
+                rightNode = cur;
+                if (cur->right) {
+                    q.push(cur->right);
+                    q.push(cur->left);
+                }
+            }
+        }
+
+        return root;
+    }
+};
