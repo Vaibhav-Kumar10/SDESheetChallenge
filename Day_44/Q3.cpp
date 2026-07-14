544243b7daecc5a8006e9a3ebce2e720fa479775
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-07-14 19:34:23 +0530

    Create Q3.cpp

---
+++ Day_44/Q3.cpp
@@ -0,0 +1,42 @@
+// Problem: Kth Largest Element in a Stream
+// Link: https://leetcode.com/problems/kth-largest-element-in-a-stream/
+class KthLargest {
+public:
+    vector<int> stream;
+    int k;
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for (int num : nums) {
+            stream.push_back(num);
+        }
+        sort(stream.begin(), stream.end());
+    }
+
+    int add(int val) {
+        int index = getIndex(val);
+        // Add val to correct position
+        stream.insert(stream.begin() + index, val);
+        return stream[stream.size() - k];
+    }
+
+private:
+    int getIndex(int val) {
+        int left = 0;
+        int right = stream.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int midValue = stream[mid];
+            if (midValue == val)
+                return mid;
+            if (midValue > val) {
+                // Go to left half
+                right = mid - 1;
+            } else {
+                // Go to right half
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
