2c82f74a6aec678e7595554a1742bac34ca81673
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-07-16 20:44:02 +0530

    Create Q2.cpp

---
+++ Day_46/Q2.cpp
@@ -0,0 +1,22 @@
+// Problem: Distinct Numbers in Each Subarray
+// Link: https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+class Solution {
+  public:
+	void dfsRecursive(int src, vector<vector<int>> & adj,
+	        vector<int>& visited, vector<int>& ansDFS) {
+		visited[src] = 1;
+		ansDFS.push_back(src);
+		for (int neighbour : adj[src]) {
+			if (!visited[neighbour]) {
+				dfsRecursive(neighbour, adj, visited, ansDFS);
+			}
+		}
+	}
+	vector<int> dfs(vector<vector<int>> & adj) {
+		int v = adj.size(), src = 0;
+		vector<int> ansDFS;
+		vector<int> visited (v, 0);
+		dfsRecursive(src, adj, visited, ansDFS);
+		return ansDFS;
+	}
+};
