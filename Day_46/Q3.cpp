f370acf6ed8b4445435b75f659f71694ee42056e
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-07-16 20:43:58 +0530

    Create Q3.cpp

---
+++ Day_46/Q3.cpp
@@ -0,0 +1,28 @@
+// Problem: Distinct Numbers in Each Subarray
+// Link: https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+class Solution {
+  public:
+	vector<int> bfs(vector<vector<int>> &adj) {
+		// No. of vertices
+		int v = adj.size(), src = 0;
+		vector<int> ansBFS;
+		vector<int> visited(v, 0);
+		queue<int> q; ;
+		q.push(src);
+		visited[src] = 1;
+		
+		while (!q.empty()) {
+			int curNode = q.front();
+			q.pop();
+			ansBFS.push_back(curNode);
+			
+			for (int neighbour : adj[curNode]) {
+				if (!visited[neighbour]) {
+					visited[neighbour] = 1;
+					q.push(neighbour);
+				}
+			}
+		}
+		return ansBFS;
+	}
+};
