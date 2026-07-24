6f73e7358a1a0ef8696cccacccfb9e3b90d07075
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-07-24 18:55:01 +0530

    Update Q2.cpp

--- Day_54/Q2.cpp
+++ Day_54/Q2.cpp
@@ -1,48 +1,21 @@
 // Problem: Coin Change II
-// Link: https://leetcode.com/problems/minimum-path-sum/
+// Link: https://leetcode.com/problems/coin-change-ii/
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        dp[0][0] = grid[0][0];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = grid[i][j];
-                    continue;
-                }
-                int up = INT_MAX, left = INT_MAX;
-                if (i > 0) {
-                    up = grid[i][j] + dp[i - 1][j];
-                }
-                if (j > 0) {
-                    left = grid[i][j] + dp[i][j - 1];
-                }
-                dp[i][j] = min(up, left);
-            }
+    int f(int ind, int target, vector<int>& arr, vector<vector<int>>& dp) {
+        if (ind == 0) {
+            return (target % arr[ind] == 0 ? 1 : 0);
         }
-        return dp[n - 1][m - 1];
+        if (dp[ind][target] != -1) {
+            return dp[ind][target];
+        }
+        int not_take = f(ind - 1, target, arr, dp);
+        int take = target >= arr[ind] ? f(ind, target - arr[ind], arr, dp) : 0;
+        return dp[ind][target] = take + not_take;
     }
-    /*
-    int f(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-        if (i == 0 && j == 0) {
-            return grid[i][j];
-        }
-        if (i < 0 || j < 0) {
-            return 1e9;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int up = grid[i][j] + f(i - 1, j, grid, dp);
-        int left = grid[i][j] + f(i, j - 1, grid, dp);
-        return dp[i][j] = min(up, left);
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        return f(n - 1, amount, coins, dp);
     }
-    int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n - 1, m - 1, grid, dp);
-    }
-    */
 };
